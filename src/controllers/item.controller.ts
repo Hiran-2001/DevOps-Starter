06864f80095b4412cf524407dcc867e70015cdc1
Hiran Raj <hirankvlr@gmail.com>
2024-10-26 02:46:36 +0530

    added a new api

--- src/controllers/item.controller.ts
+++ src/controllers/item.controller.ts
@@ -1,27 +1,49 @@
-import { ItemModel } from '../models/item.model' 
+import mongoose from 'mongoose'
+import { ItemModel } from '../models/item.model'
 import logger from '../utils/logger'
-import {Request,Response} from 'express'
+import { Request, Response } from 'express'
 
 
 export class ItemController {
-    public static async getItems(requset: Request, response:Response): Promise<void>{
+    public static async getItems(requset: Request, response: Response): Promise<void> {
         try {
-            const items = await ItemModel.find().sort({createdAt: -1})
+            const items = await ItemModel.find().sort({ createdAt: -1 })
             response.json(items)
         } catch (error) {
             logger.error('Error fetching items:', error)
-            response.status(500).json({error: 'Internal server error'})
+            response.status(500).json({ error: 'Internal server error' })
         }
     }
 
-    public static async createItem(request: Request, response: Response){
+    public static async getSingleItem(request: Request, response: Response): Promise<any> {
+        try {
+            const id = request.params.id;
+
+            if (!mongoose.Types.ObjectId.isValid(id)) {
+                return response.status(404).json({ error: 'Invalid ID format' });
+            }
+
+            const item = await ItemModel.findById(id);
+
+            if (!item) {
+              return  response.status(401).json('Item not found');
+            }
+
+          return  response.status(200).json(item)
+        } catch (error) {
+            logger.error('Error fetching item:', error)
+           return response.status(500).json({ error: 'Internal server error' })
+        }
+    }
+
+    public static async createItem(request: Request, response: Response) {
         try {
             const item = new ItemModel(request.body)
             await item.save()
             response.status(201).json(item)
         } catch (error) {
             logger.error('Error creating item:', error)
-            response.status(400).json({error: "Invalid item data"})
+            response.status(400).json({ error: "Invalid item data" })
         }
     }
 }
--- src/routes/item.routes.ts
+++ src/routes/item.routes.ts
@@ -4,6 +4,7 @@ import { ItemController } from '../controllers/item.controller';
 const router = Router();
 
 router.get('/', ItemController.getItems);
+router.get('/:id',ItemController.getSingleItem)
 router.post('/', ItemController.createItem);
 
 export default router;
